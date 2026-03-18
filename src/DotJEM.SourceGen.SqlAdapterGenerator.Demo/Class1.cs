1d3f6a756199e3f5526f5008fad7bb7b8fafa713
jmd <jens.melgaard@systematic.com>
2026-03-18 14:12:08 +0100

    proto

--- src/DotJEM.SourceGen.SqlAdapterGenerator.Demo/Class1.cs
+++ src/DotJEM.SourceGen.SqlAdapterGenerator.Demo/Class1.cs
@@ -1,7 +1,58 @@
-﻿namespace DotJEM.SourceGen.SqlAdapterGenerator.Demo
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace DotJEM.SourceGen.SqlAdapterGenerator.Demo
 {
-    public class Class1
+    public class DataTableAdapter(string schema, string dataTableName, SqlConnection connection)
     {
         
+
+        public readonly record struct RecordName(Guid Id, string ContentType, long Version, DateTime Created, DateTime Updated, string CreatedBy, string UpdatedBy, string Data);
+        /*
+         *--method:Select<RecordName> --single:true
+           SELECT [Id]
+               ,[ContentType]
+               ,[Version]
+               ,[Created]
+               ,[Updated]
+               ,[CreatedBy]
+               ,[UpdatedBy]
+               ,[Data]
+           FROM [@{schema}].[@{data_table_name}]
+           WHERE [Id] = @id;
+         *
+         *
+         */
+        public RecordName Select(Guid id)
+        {
+            const string cmd = """
+                               SELECT [Id]
+                                   ,[ContentType]
+                                   ,[Version]
+                                   ,[Created]
+                                   ,[Updated]
+                                   ,[CreatedBy]
+                                   ,[UpdatedBy]
+                                   ,[Data]
+                               FROM [@{schema}].[@{data_table_name}]
+                               WHERE [Id] = @id;
+                               """;
+
+            SqlCommand command = new(cmd, connection);
+
+
+            foreach ((string name, SqlDbType sqlDbType, object value) in parameters)
+                command.Parameters.Add(name, sqlDbType).Value = value;
+            return new SqlServerCommand(connection, command);
+
+
+            SqlDataReader reader = null; // execute command and get reader 
+            reader.
+
+
+            return default;
+        }
+
     }
 }
